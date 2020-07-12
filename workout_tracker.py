f724ef7521a20ddc34d0737b1fae800743b4a3d2
Marshall Ferguson <moferg01@louisville.edu>
2020-07-12 09:59:57 -0400

    outputs workout data to a .csv file

--- workout_tracker.py
+++ workout_tracker.py
@@ -1,10 +1,14 @@
 # Workout Tracker - Marshall Ferguson - 7/2020
 
+# Imports
+
+import csv
+
 # TODO - take input from the command line
     # TODO - ask the user the name of exercise, sets, reps, and weight
     # TODO - calculate the volume lifted, the total sets, and total reps
 
-print("Welcome to the workout Tracker!")
+print("Welcome to the Workout Tracker!")
 
 name = input("Please enter the name of the exercise you want to add next:     ")
 sets = input("How many sets did you perform?     ")
@@ -20,4 +24,9 @@ print("You performed {} sets of {} reps of {} at {} lbs".format(sets, reps, name
 print("You performed a total of {} reps and lifted a total volume of {} lbs".format(total_reps, volume))
 
 # TODO - output to a csv file
+
+output_file = open(".\\workout_data.csv", "a", newline="")
+output_writer = csv.writer(output_file)
+output_writer.writerow([name, sets, reps, weight, total_reps, volume])
+
 # TODO - visualize data
