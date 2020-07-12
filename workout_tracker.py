4d22d1fb327ce3de2c3d09281c2624fa0a139d47
Marshall Ferguson <moferg01@louisville.edu>
2020-07-12 03:32:20 -0400

    program takes input from command line

--- workout_tracker.py
+++ workout_tracker.py
@@ -1,7 +1,23 @@
 # Workout Tracker - Marshall Ferguson - 7/2020
 
 # TODO - take input from the command line
-    # TODO - ask the user the name of exercise, sets, reps and weight
-    # TODO - calculate the volume lifted, the total set, and total reps
+    # TODO - ask the user the name of exercise, sets, reps, and weight
+    # TODO - calculate the volume lifted, the total sets, and total reps
+
+print("Welcome to the workout Tracker!")
+
+name = input("Please enter the name of the exercise you want to add next:     ")
+sets = input("How many sets did you perform?     ")
+sets = int(sets)
+reps = input("How many reps did you perform each set?     ")
+reps = int(reps)
+weight = input("How much weight did you lift each rep? (in lbs)     ")
+weight = int(weight)
+total_reps = reps * sets
+volume = total_reps * weight
+
+print("You performed {} sets of {} reps of {} at {} lbs".format(sets, reps, name, weight))
+print("You performed a total of {} reps and lifted a total volume of {} lbs".format(total_reps, volume))
+
 # TODO - output to a csv file
 # TODO - visualize data
