a810b48ee26951b13fbccaec30ae38621f44c1f0
Marshall Ferguson <moferg01@louisville.edu>
2020-07-12 02:52:52 -0400

    added TODOs

--- workout_tracker.py
+++ workout_tracker.py
@@ -1,1 +1,7 @@
 # Workout Tracker - Marshall Ferguson - 7/2020
+
+# TODO - take input from the command line
+    # TODO - ask the user the name of exercise, sets, reps and weight
+    # TODO - calculate the volume lifted, the total set, and total reps
+# TODO - output to a csv file
+# TODO - visualize data
