d3c2473d9ba91ba0d573701c937d5c1639174f44
Marshall Ferguson <moferg01@louisville.edu>
2020-07-13 09:03:11 -0400

    keeps track of exercise names and volumes of each exercise

--- workout_tracker.py
+++ workout_tracker.py
@@ -12,11 +12,14 @@ import matplotlib.pyplot as plt
 print("Welcome to the Workout Tracker!")
 output_file = open(".\\workout_data.csv", "a", newline="")
 output_writer = csv.writer(output_file)
+workout_exercises = []
+exercise_volumes = []
 print("Please enter your information for your workout.")
 while True:
     user_input = input("Enter 'done' when you are done adding workout information, or press enter to continue.\n")
     if user_input.lower() != "done":
         name = input("Please enter the name of the exercise you want to add next:     ")
+        workout_exercises.append(name)
         sets = input("How many sets did you perform?     ")
         sets = int(sets)
         reps = input("How many reps did you perform each set?     ")
@@ -25,11 +28,14 @@ while True:
         weight = int(weight)
         total_reps = reps * sets
         volume = total_reps * weight
+        exercise_volumes.append(volume)
         print("You performed {} sets of {} reps of {} at {} lbs".format(sets, reps, name, weight))
         print("You performed a total of {} reps and lifted a total volume of {} lbs".format(total_reps, volume))
         output_writer.writerow([name, sets, reps, weight, total_reps, volume])
     else:
         break
+
+print("For your workout, you performed a total of {} exercises and lifted a total of {} lbs.".format(len(workout_exercises), sum(exercise_volumes)))
 
 # TODO - output to a csv file
 
